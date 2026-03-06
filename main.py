42d4bbfae50cd62265b4fd2bbca93cedad4312f4
SnowyDev47 <snowy42420@gmail.com>
2026-03-06 18:14:51 +0100

    1.0.1 --> Added clock and dt variables and game set to 60 fps

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,6 +21,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()

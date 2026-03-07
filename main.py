2382cbc4ba3fe1f27e2d996a4fd07ef6e4843930
SnowyDev47 <snowy42420@gmail.com>
2026-03-07 16:33:22 +0100

    1.0.2 --> Added 2 files and added player instance

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -14,14 +15,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player_instance = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
         dt = clock.tick(60) / 1000
+        screen.fill("black")
+        player_instance.draw(screen)  
+        pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
